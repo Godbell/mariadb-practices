8075f9ee1f470eb8276eda9d2a700a6cd00e89a0
Godbell <12191579@inha.edu>
2024-12-10 15:08:14 +0900

    Fix: correct practice02

--- sql-practices/practice02.sql
+++ sql-practices/practice02.sql
@@ -14,7 +14,7 @@ FROM salaries;
 -- 마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력하세요
 -- 예) 2014년 07월 10일
 SELECT 
-	DATE_FORMAT(hire_date, "%Y년 %m월 %d일")
+	DATE_FORMAT(hire_date, "%Y년 %m월 %d일") AS "마지막으로 신입사원이 들어온 날"
 FROM employees
 ORDER BY hire_date DESC
 LIMIT 1;
@@ -23,18 +23,24 @@ LIMIT 1;
 -- 가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력하세요.
 -- 예) 2014년 07월 10일
 SELECT 
-	DATE_FORMAT(hire_date, "%Y년 %m월 %d일")
+	DATE_FORMAT(hire_date, "%Y년 %m월 %d일") AS "최장근속자 입사일"
 FROM employees
 ORDER BY hire_date ASC
 LIMIT 1;
 
 -- 문제4.
 -- 현재, 이 회사의 평균 연봉은 얼마입니까?
-SELECT AVG(salary) FROM salaries;
+SELECT AVG(salary) AS "평균 연봉"
+FROM salaries
+WHERE to_date = '9999-01-01';
 
 -- 문제5.
 -- 현재, 이 회사의 최고/최저 연봉은 각각 얼마입니까?
-SELECT MAX(salary), MIN(salary) FROM salaries;
+SELECT
+	MAX(salary) AS "최고 연봉",
+	MIN(salary) AS "최저 연봉"
+FROM salaries
+WHERE to_date = '9999-01-01';
 
 -- 문제6.
 -- 현재, 근무중인 사원 중 나이가 제일 어린 사원과 제일 많은 사원의 나이를 각각 출력하세요.
